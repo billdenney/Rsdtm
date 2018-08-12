7ace133cb0e1603df8bd85e0d58f27cc11204180
Bill Denney <wdenney@humanpredictions.com>
2018-08-12 15:50:25 -0400

    Detect RELREC and supplementary domains; add verbose messaging

--- R/detect_domain.R
+++ R/detect_domain.R
@@ -4,10 +4,13 @@
 #' @param filename The filename associated with \code{x}
 #' @return A character scalar with the name of the domain
 #' @export
-detect_domain <- function(x, filename) {
-  domain_column_name <- intersect(c("DOMAIN", "ADDOMAIN"), names(x))
+detect_domain <- function(x, filename, verbose=TRUE) {
+  if ("RELID" %in% names(x)) {
+    return("RELREC")
+  }
+  domain_column_name <- intersect(c("DOMAIN", "ADDOMAIN", "RDOMAIN"), names(x))
   if (length(domain_column_name) == 0) {
-    domain_type <- "filename"
+    name_type <- "filename"
   } else if (length(domain_column_name) == 1) {
     name_type <- "domain"
   } else {
@@ -21,10 +24,17 @@ detect_domain <- function(x, filename) {
     name_type <- "filename"
   }
   if (name_type %in% "domain") {
-    make_sdtm_domain_from_domain(x)
+    ret <- make_sdtm_domain_from_domain(x)
+    if (verbose) {
+      message("Detected ", ret, " from domain column.")
+    }
   } else if (name_type %in% "filename") {
-    make_sdtm_domain_from_filename(filename)
+    ret <- make_sdtm_domain_from_filename(filename)
+    if (verbose) {
+      message("Detected ", ret, " from filename.")
+    }
   }
+  ret
 }
 
 #' Simplify a filename down to the SDTM list name that should be provided.
@@ -34,20 +44,24 @@ make_sdtm_domain_from_filename <- function(filenames = NULL) {
   if (is.null(filenames)) {
     stop("filenames may not be NULL.")
   } else {
-    base::basename(tools::file_path_sans_ext(filenames))
+    toupper(base::basename(tools::file_path_sans_ext(filenames)))
   }
 }
 
 #' Extract the domain from a dataset.
 #' @param data The data with a DOMAIN column for extraction
 make_sdtm_domain_from_domain <- function(data) {
-  domain_column_name <- intersect(c("DOMAIN", "ADDOMAIN"), names(data))
+  domain_column_name <- intersect(c("DOMAIN", "ADDOMAIN", "RDOMAIN"), names(data))
   ret <- unique(data[[domain_column_name]])
   if (length(ret) != 1) {
     stop(
       "More than one DOMAIN value cannot be present to make an SDTM list name. DOMAIN values found are: ",
       paste(ret, collapse = ", ")
     )
+  }
+  if (domain_column_name %in% "RDOMAIN") {
+    # Handle supplementary datasets
+    ret <- paste0("SUPP", ret)
   }
   ret
 }
