a3d04556195dadf7d3de1378e3ff642a8d034664
Kiranmai Ganji <36434562+kiranmaiganji@users.noreply.github.com>
2020-07-24 17:07:06 +0530

    generate_example_dataverse.R
    Added all the datasets

--- data-raw/SDTM_example_files/generate_example_dataverse.R
+++ data-raw/SDTM_example_files/generate_example_dataverse.R
@@ -1,14 +1,42 @@
 library(devtools)
 library(usethis)
 
-# Generate example SDTM datasets here using the Harvard Dataverse datasets.  The
-# name of the dataset should be "sdtm_example_dataverse_" followed by the SDTM
-# domain.  The long name prefix (sdtm_example_dataverse_) anticipates that
-# additional example may be available in the future.
-
+# AE Domain
 sdtm_example_dataverse_ae <-
-  read.csv(file="Harvard directory/ae.csv", stringsAsFactors=FALSE)
-# Confirm that all columns match the expected SDTM type (e.g. character vs
-# numeric) here.  To know the class, refer to the SDTM standard for the domain.
+  read.csv(file="Harvard directory/ae_raw.csv", stringsAsFactors=FALSE)
 sdtm_example_dataverse_ae$AESPID <- as.character(sdtm_example_dataverse_ae$AESPID)
 use_data(sdtm_example_dataverse_ae)
+
+#DM Domain
+sdtm_example_dataverse_dm <-
+  read.csv(file="Harvard directory/dm.csv", stringsAsFactors=FALSE)
+sdtm_example_dataverse_dm$SUBJID <- as.character(sdtm_example_dataverse_dm$SUBJID)
+use_data(sdtm_example_dataverse_dm)
+
+#EG Domain
+sdtm_example_dataverse_eg <-
+  read.csv(file="Harvard directory/eg.csv", stringsAsFactors=FALSE)
+use_data(sdtm_example_dataverse_eg)
+
+#LB Domain
+sdtm_example_dataverse_lb <-
+  read.csv(file="Harvard directory/lb.csv", stringsAsFactors=FALSE)
+sdtm_example_dataverse_lb$LBORNRLO <- as.character(sdtm_example_dataverse_lb$LBORNRLO)
+sdtm_example_dataverse_lb$LBORNRHI <- as.character(sdtm_example_dataverse_lb$LBORNRHI)
+use_data(sdtm_example_dataverse_lb)
+
+#MH Domain
+sdtm_example_dataverse_mh <-
+  read.csv(file="Harvard directory/mh.csv", stringsAsFactors=FALSE)
+use_data(sdtm_example_dataverse_mh)
+
+#PE Domain
+sdtm_example_dataverse_pe <-
+  read.csv(file="Harvard directory/pe.csv", stringsAsFactors=FALSE)
+use_data(sdtm_example_dataverse_pe)
+
+#VS Domain
+sdtm_example_dataverse_vs <-
+  read.csv(file="Harvard directory/vs.csv", stringsAsFactors=FALSE)
+use_data(sdtm_example_dataverse_vs)
+
